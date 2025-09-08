058c7d09e11311e29df63cb36eab7e0ac14b2a13
QAIS71 <kas7746kebe@gmail.com>
2025-09-08 20:55:12 +0300

    server.js

--- server.js
+++ server.js
@@ -2702,8 +2702,7 @@ app.delete('/api/group/:groupId/leave', async (req, res) => {
 
 // NEW: Import and use marketing routes
 const marketingRoutes = require('./marketingRoutes'); 
-app.use('/api/marketing', marketingRoutes(projectDbPools, projectSupabaseClients, upload, BACKEND_DEFAULT_PROJECT_ID, sendOneSignalNotification, FRONTEND_URL));
-
+app.use('/api/marketing', marketingRoutes(projectDbPools, projectSupabaseClients, upload, BACKEND_DEFAULT_PROJECT_ID, sendOneSignalNotification, FRONTEND_URL, stripe));
 // ==== بداية كود نقطة نهاية حفظ اشتراك الإشعارات ====
 app.post('/api/subscribe', async (req, res) => {
     const { subscription, userId } = req.body;
